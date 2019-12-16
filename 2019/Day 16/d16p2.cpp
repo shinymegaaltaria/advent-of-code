1250db512164d6ed57c2d63b6ca8f9b8e2cb6ada
shinymegaaltaria <chanryon@gmail.com>
2019-12-16 19:59:46 +0800

    Day 16 wheeeeeee

--- 2019/Day 16/d16p2.cpp
+++ 2019/Day 16/d16p2.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int main(){
     ifstream phasein ("day16_part2_in.txt");
+    ofstream debug ("day16_debug.txt");
     string phase;
     phasein >> phase;
     int skip = 0;
@@ -14,32 +15,34 @@ int main(){
     int multiplier[4] = {0, 1, 0, -1};
     int size = phase.size();
     for(int i = 0 ; i < 100 ; i++){
+    	cout << "Rotation #" << i << endl;
+    	//debug << phase << endl;
+    	//debug << "Rotation #" << i << endl;
         string new_phase = phase;
         vector <int> og;
         vector <int> ps;
         og.push_back(0);
         for(int j = 0 ; j < size ; j++){
-            og.push_back(phase[i]-48);
+            og.push_back(phase[j]-48);
         }
         ps = og;
         for(int j = 1 ; j < size + 1 ; j++){
             ps[j] = og[j] + ps[j - 1];
         }
+        //for(int j = 0 ; j < size + 1 ; j++) debug << ps[j] << " ";
+        //debug << endl;
         for(int j = 1 ; j < size + 1; j++){
-            cout << j << " ok" << endl;
             int tmp = 0;
-            for(int k = 1 ; k <= ((size - 1) / j) + 1 ; k++){
-                int r = (k + 1) * j - 1;
-                if(size < r){
-                    r = size;
-                }
+            for(int k = 1 ; k <= size/j ; k++){
+                int r = min(size,(k + 1) * j - 1);
                 if(k % 4 == 1){
-                    tmp += ps[r] - ps[k*j-1];
+                    tmp += (ps[r] - ps[k*j-1]);
                 }
                 else if(k % 4 == 3){
-                    tmp -= ps[r] - ps[k*j-1];
+                    tmp -= (ps[r] - ps[k*j-1]);
                 }
             }
+            debug << tmp << endl;
             if(tmp < 0){
                 tmp = -1 * tmp;
             }
@@ -48,5 +51,6 @@ int main(){
         phase = new_phase;
     }
     for(int i = skip; i < skip + 8 ; i++) cout << phase[i];
+    //cout << phase << endl;
     //to determine ((i+1)%(4*pos))/pos
 }
